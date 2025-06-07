b0ea0f1f588d5bf885c31c32e24adbe40cb92869
GunJu0415 <66293019+GunJu0415@users.noreply.github.com>
2025-06-07 23:03:01 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+int t;
+int main()
+{
+	cin >> t;
+	while (t--)
+	{
+		int a, b;
+		char c;
+		cin >>a >> c >> b;
+		cout << a + b << "\n";
+	}
+	return 0;
+}
