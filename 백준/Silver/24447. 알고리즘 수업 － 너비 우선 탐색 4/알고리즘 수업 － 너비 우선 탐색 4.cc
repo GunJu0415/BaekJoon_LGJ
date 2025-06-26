42e96a318e2dca1486bfbb74efa278e27d792d04
GunJu0415 <66293019+GunJu0415@users.noreply.github.com>
2025-06-26 13:24:53 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 4, Time: 80 ms, Memory: 9228 KB -BaekjoonHub

--- 백준/Silver/24447. 알고리즘 수업 － 너비 우선 탐색 4/알고리즘 수업 － 너비 우선 탐색 4.cc
+++ 백준/Silver/24447. 알고리즘 수업 － 너비 우선 탐색 4/알고리즘 수업 － 너비 우선 탐색 4.cc
@@ -3,69 +3,70 @@
 #include<vector>
 #include<queue>
 
+
 using namespace std;
-typedef long long ll;
+typedef long long int ll;
 
 int n, m, r;
 vector<int> v[100004];
 int dist[100004];
 int depth[100004];
 ll ret = 0;
-
 void bfs(int x)
 {
-    queue<int> q;
-    q.push(x);
-    dist[x] = 0;
-
-    int order = 1;
-    depth[x] = order++;
-
-    while (!q.empty())
-    {
-        int now = q.front();
-        q.pop();
-
-        for (int next : v[now])
-        {
-            if (dist[next] != -1) continue;
-
-            dist[next] = dist[now] + 1;
-            depth[next] = order++;
-            q.push(next);
-        }
-    }
+	int temp = 1;
+	depth[x] = temp++;
+	queue<int>q;
+	q.push(x);
+	dist[x] = 0;
+	while (!q.empty())
+	{
+		int x = q.front();
+		q.pop();
+		
+		for (int i : v[x])
+		{
+			if (dist[i] != 1e9)
+			{
+				continue;
+			}
+			depth[i] = temp++;
+			dist[i] = dist[x] + 1;
+			q.push(i);
+		}
+	}
 }
-
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
-    cin >> n >> m >> r;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> m >> r;
 
-    for (int i = 0; i < m; i++)
-    {
-        int a, b;
-        cin >> a >> b;
-        v[a].push_back(b);
-        v[b].push_back(a);
-    }
+	fill(&dist[0], &dist[n + 1], 1e9);
+	for (int i = 0; i < m; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
 
-    for (int i = 1; i <= n; i++)
-    {
-        sort(v[i].begin(), v[i].end());  // 오름차순 방문
-        dist[i] = -1;
-    }
 
-    bfs(r);
+	for (int i = 1; i <= n; i++)
+	{
+		sort(v[i].begin(), v[i].end());
+	}
+	bfs(r);
 
-    for (int i = 1; i <= n; i++)
-    {
-        if (dist[i] == -1) continue;
-        ret += 1LL * dist[i] * depth[i];
-    }
-
-    cout << ret << '\n';
+	for (int i = 1; i <= n; i++)
+	{
+		if (dist[i] == 1e9)
+		{
+			continue;
+		}
+		ret += 1LL *depth[i] * dist[i];
+	}
+	cout << ret;
 }
