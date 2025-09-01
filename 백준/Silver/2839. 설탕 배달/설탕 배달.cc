c6ec26f27c4711cb20107436fde7bba67df647dc
GunJu0415 <66293019+GunJu0415@users.noreply.github.com>
2025-09-01 09:52:43 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,24 +1,64 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
+
 
 using namespace std;
 
 int n;
-int cnt = -1;
-int mn = 1234897;
+int sum = 0;
+int cnt = 0;
+int ret =  1e9;
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	cin >> n;
-	for (int i = 0; i < n; i++)
+
+	if (n < 5)
 	{
-		for (int j = 0; j < n; j++)
+		if (n % 3 == 0)
 		{
-			if (i * 5 + j * 3 == n && mn > i+j )
-			{
-				mn = i + j;
-				cnt = i + j;
-			}
+			cnt = 1;
+			cout << cnt;
+			return 0;
+		}
+		else
+		{
+			cout << -1;
+			return 0;
 		}
 	}
-	cout << cnt;
+
+	for (int i = 0; i * 5 <= n; i ++)
+	{
+		
+		cnt = i;
+		sum = 5 * i;
+		//cout << sum << " ";
+		for (int j = 0; j * 3 + sum <=  n; j ++)
+		{
+			//cnt;
+			int temp = sum;
+			temp += j * 3;
+
+			if (temp == n)
+			{
+				ret = min(cnt + j, ret);
+			}
+			//cout << j << " ";
+
+		}
+
+
+	}
+	if (ret == 1e9)
+	{
+		cout << -1;
+		return 0;
+	}
+	cout << ret;
 	return 0;
 }
