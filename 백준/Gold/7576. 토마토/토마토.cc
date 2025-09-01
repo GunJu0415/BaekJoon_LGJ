0a3e0801be2ecf872082af2ec0badae560606d1c
GunJu0415 <66293019+GunJu0415@users.noreply.github.com>
2025-09-01 10:10:55 +0900

    [Gold V] Title: 토마토, Time: 76 ms, Memory: 11240 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,64 +1,77 @@
 #include<iostream>
-#include<vector>
 #include<queue>
+#include<vector>
 #include<algorithm>
+
 
 using namespace std;
 
-int dx[4] = { 0, 1, 0, -1 };
-int dy[4] = { 1, 0, -1, 0 };
 
+int dy[4] = { 0, 1, 0, -1 };
+int dx[4] = { 1, 0, -1, 0 };
 
-queue<pair<int, int>> q;
+int n, m;
+int cnt = 0;
+int ret = 0;
+int ans = 0;
 vector<vector<int>>v;
-bool visit[1004][1004];
+bool dist[1004][1004];
+queue<pair<int, int>>q;
 
-
-int n, m, z = 0;
 
 void bfs()
 {
-
 	while (!q.empty())
 	{
-		int x = q.front().first;
-		int y = q.front().second;
-		visit[x][y] = true;
+		int y = q.front().first;
+		int x = q.front().second;
 		q.pop();
-
-
+		ret++;
 		for (int i = 0; i < 4; i++)
 		{
-			int nx = x + dx[i];
 			int ny = y + dy[i];
+			int nx = x + dx[i];
 
-			if (nx >= m || nx < 0 || ny >= n || ny < 0)
+			if (ny >= m || ny < 0 || nx >= n || nx < 0)
 			{
 				continue;
 			}
-
-			if (v[nx][ny] != 0)
+			if (v[ny][nx] != 0)
 			{
 				continue;
 			}
 			
-			if (!visit[nx][ny])
-			{
-				visit[nx][ny] = true;
-				v[nx][ny] = v[x][y] + 1;
-				q.push(make_pair(nx, ny));
-			}
+			v[ny][nx] = v[y][x] + 1;
+			ans = max(v[ny][nx], ans);
+			q.push({ ny, nx });
+
+
 		}
+
 	}
 
+}
+void print()
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << v[i][j] << " ";
+		}
+		cout << "\n";
+	}
 }
 
 int main()
 {
-	int a = 0, b = 0;
-	cin >> n >> m;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	cin >> n >> m;
 	v.resize(m, vector<int>(n));
+
 	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++)
@@ -66,35 +79,35 @@ int main()
 			cin >> v[i][j];
 			if (v[i][j] == 1)
 			{
-				z++;
-				q.push(make_pair(i, j));
+				q.push({ i, j });
+				cnt++;
 			}
-		}
-	}
-	bfs();
-
-	int max = 1;
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
 			if (v[i][j] == 0)
 			{
-				max = 0;
-				break;
+				cnt++;
 			}
-			if (max <= v[i][j])
-			{
-				max = v[i][j];
-			}
-		}
-		if (max == 0)
-		{
-			break;
 		}
 	}
 
-	max -= 1;
-	cout << max;
+	bfs();
+	//print();
+	if (cnt == ret)
+	{
+		if (ans == 0)
+		{
+			cout << ans;
+		}
+		else
+		{
+
+			cout << ans - 1;
+		}
+	}
+	else
+	{
+
+		cout << -1;
+	}
+
 	return 0;
 }
